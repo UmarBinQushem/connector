2bcff2007fb08d01a624d0d9b70a5fcbb2d35491
UmarBinQushem <ninjabd2@gmali.com>
2014-02-23 16:02:01 +0800

    Initial Commit for this experimental repo

---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :name, :phone, :address, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+People::Application.config.secret_key_base = 'cd527e4eb4a774dbac78c9b0d5cc53d72a43e540a2ee2eb0d0ccc235a8e79759e48fd7e8ae830f3aa69bb3965edf1646b7389a3dc7546876aea947a65e9190ff'
